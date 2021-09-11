fb18fdaaa449e5dfcb115678317de098039027c3
k51011 <k5-big1011@docomo.ne.jp>
2021-09-11 19:59:26 +0900

    カテゴリシーダーの作成 #4

---
+++ app/Category.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    const UPDATED_AT = null;
+    const CREATED_AT = null;
+    protected $guarded = ['id'];
+}
